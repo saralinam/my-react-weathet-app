32e1b7e96f6094883bd3eeb66836b8cbea06f142
Sara-mwk <106980251+Sara-mwk@users.noreply.github.com>
2022-10-26 12:28:08 -0500

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 
 export default function App() {
   return (<div className="App">
-      Weather App
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Sara Mouwakee and is {" "}
+      <a href="https://github.com/Sara-mwk/my-react-weathet-app"
+      target="_blank"
+
+>open saourced on GitHub</a>
+      </footer>
     </div>);
 }
 
