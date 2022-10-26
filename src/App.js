e26cf3ca6239ee5718356c29710a070e5b6dbf77
Sara-mwk <106980251+Sara-mwk@users.noreply.github.com>
2022-10-26 13:09:49 -0500

    removed logo.svg

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 export default function App() {
   return (<div className="App">
       <h1> Weather App</h1>
-      <footer>
-        This project was coded by Sara Mouwakee and is {" "}
-      <a href="https://github.com/Sara-mwk/my-react-weathet-app"
-      target="_blank"
 
->open saourced on GitHub</a>
-      </footer>
     </div>);
 }
 
